78389fd05a6f58f9ea82707b4de1014a8cf912d7
952269089 <952269089@qq.com>
2017-06-08 21:38:09 +0800

    提交

---
+++ app/src/main/java/com/example/administrator/ms/MainActivity5.java
@@ -0,0 +1,15 @@
+package com.example.administrator.ms;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e("嫂子8097650000","嫂子9999");
+    }
+}
