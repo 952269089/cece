727c3e786f699ba0a733e9feafa3e6b3b569494a
952269089 <952269089@qq.com>
2017-06-08 20:37:12 +0800

    嫂子7

---
+++ app/src/main/java/com/example/administrator/ms/MainActivity2.java
@@ -0,0 +1,15 @@
+package com.example.administrator.ms;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e("嫂子7","嫂子777");
+    }
+}
